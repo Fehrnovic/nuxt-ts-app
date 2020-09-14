61d390450cd88a809dbed2479675f04dfb15508f
Oscar Fehrn <oscarfehrn@gmail.com>
2020-09-14 17:38:27 +0200

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,100 @@
+export default {
+  /*
+   ** Nuxt rendering mode
+   ** See https://nuxtjs.org/api/configuration-mode
+   */
+  mode: "spa",
+  /*
+   ** Nuxt target
+   ** See https://nuxtjs.org/api/configuration-target
+   */
+  target: "server",
+  /*
+   ** Headers of the page
+   ** See https://nuxtjs.org/api/configuration-head
+   */
+  head: {
+    title: process.env.npm_package_name || "",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      }
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+  },
+  /*
+   ** Global CSS
+   */
+  css: [],
+  /*
+   ** Plugins to load before mounting the App
+   ** https://nuxtjs.org/guide/plugins
+   */
+  plugins: [],
+  /*
+   ** Auto import components
+   ** See https://nuxtjs.org/api/configuration-components
+   */
+  components: true,
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: ["@nuxt/typescript-build"],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    "@nuxtjs/axios",
+    "@nuxtjs/auth-next"
+  ],
+  /*
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
+  axios: {
+    baseURL: "http://localhost:5000"
+  },
+  /*
+   ** Build configuration
+   ** See https://nuxtjs.org/api/configuration-build/
+   */
+  build: {},
+  auth: {
+    strategies: {
+      local: {
+        scheme: "refresh",
+        token: {
+          property: "jwtToken"
+        },
+        refreshToken: {
+          property: "refreshToken",
+          data: "refreshToken"
+        },
+        user: {
+          property: false,
+          autoFetch: true
+        },
+        endpoints: {
+          login: { url: "/auth/login", method: "post" },
+          refresh: { url: "/auth/refresh-token", method: "post" },
+          user: { url: "users/user", method: "get" },
+          logout: false
+        }
+        // autoLogout: false
+      }
+    },
+    redirect: {
+      login: "/login",
+      logout: "/login",
+      home: false
+    }
+  },
+  router: {
+    middleware: ["auth"]
+  }
+};
---
+++ pages/login/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <form @submit.prevent="userLogin">
+      <div>
+        <label>Username</label>
+        <input type="text" v-model="login.username" />
+      </div>
+      <div>
+        <label>Password</label>
+        <input type="text" v-model="login.password" />
+      </div>
+      <div>
+        <button type="submit">Submit</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      login: {
+        username: "test",
+        password: "1234",
+      },
+    };
+  },
+  methods: {
+    async userLogin() {
+      try {
+        console.log(this.login);
+        let response = await this.$auth.loginWith("local", {
+          data: this.login,
+        });
+        console.log(response);
+
+        this.$router.push({ path: "/" });
+      } catch (err) {
+        console.log("hej");
+        console.log(err);
+      }
+    },
+  },
+};
+</script>
